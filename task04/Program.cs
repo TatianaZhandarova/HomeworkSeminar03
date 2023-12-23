af3b9ff81378e67304a11dbffb29ca455bdac52a
Tatiana Zhandarova <vojjtikunova@gmail.com>
2023-12-23 04:09:42 +0300

    четвертая задача

---
+++ task04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Введите натуральное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num + "";
+if(CheckNum(num)){
+    int [] array = NeedArray(num);
+    PrintArray(array);
+}
+
+bool CheckNum(int i){
+    if (i>=1 && i<=100000){
+        return true;
+    }
+    return false;
+}
+
+int [] NeedArray(int j){
+    int[] num = new int[j];
+    for(int i = 0; i < num.Length; i++){
+        if(j<10){
+        num[i]=j;
+        } 
+        if(j>9){
+            num[i] = j%10;//0,0
+            j /=10;//50,5   
+        }
+    }
+    return num;
+}
+
+void PrintArray(int[] col )
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.Write(col[position]);
+        position++;
+    }
+}
